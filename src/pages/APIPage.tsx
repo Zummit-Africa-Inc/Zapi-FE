ec669c371fd9fa6022892cdc3d1f7e6d2afb42a4
Samson Okunola <smsnmicheal@gmail.com>
2022-07-27 13:53:42 +0100

    init setup - vite react.ts

---
+++ src/pages/APIPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const APIPage:React.FC = () => {
+  return (
+    <div>APIPage</div>
+  )
+}
+
+export default APIPage
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as APIPage } from "./APIPage";
+export { default as Home } from "./Home";
+export { default as Login } from "./Login";
