e91ddacfb9f36380fae757499b9bcbd553a45e56
Samson Okunola <smsnmicheal@gmail.com>
2022-08-26 11:39:57 +0100

    added configuration page as exotic component

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,19 +1,21 @@
 import React from "react";
 
-const APIPage = React.lazy(() => import("./APIPage"))
-const Analytics = React.lazy(() => import("./Analytics/Analytics"))
-const DevDashboard = React.lazy(() => import("./DevDashboard/DevDashboard"))
-const ForgotPassword = React.lazy(() => import("./ForgotPassword"))
-const Home = React.lazy(() => import("./Home"))
-const Login = React.lazy(() => import("./Login"))
-const LoginHistory = React.lazy(() => import("./LoginHistory"))
-const Otp = React.lazy(() => import("./Otp"))
-const Signup = React.lazy(() => import('./Signup'))
-const SuccessPage = React.lazy(() => import("./SuccessPage"))
-const UserProfile = React.lazy(() => import("./UserProfile"))
+// ! Please add all pages as exotic components as below
 
+const Analytics = React.lazy(() => import("./Analytics/Analytics"));
+const APIPage = React.lazy(() => import("./APIPage"));
+const Configuration = React.lazy(() => import("./Configuration"));
+const DevDashboard = React.lazy(() => import("./DevDashboard/DevDashboard"));
+const ForgotPassword = React.lazy(() => import("./ForgotPassword"));
+const Home = React.lazy(() => import("./Home"));
+const Login = React.lazy(() => import("./Login"));
+const LoginHistory = React.lazy(() => import("./LoginHistory"));
+const Otp = React.lazy(() => import("./Otp"));
+const Signup = React.lazy(() => import('./Signup'));
+const SuccessPage = React.lazy(() => import("./SuccessPage"));
+const UserProfile = React.lazy(() => import("./UserProfile"));
 
-export { APIPage, Analytics, DevDashboard,
+export { Analytics, APIPage, Configuration, DevDashboard,
     ForgotPassword, Home, Login,
     LoginHistory, Otp, Signup,
-    SuccessPage, UserProfile}
+    SuccessPage, UserProfile };
