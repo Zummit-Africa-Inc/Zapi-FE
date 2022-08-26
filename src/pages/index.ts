572a3333119c4bd860039809d34cb599e58c1d04
Samson Okunola <smsnmicheal@gmail.com>
2022-08-26 11:35:08 +0100

    changed pages to exotic components and added a fallback

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,11 +1,19 @@
-export { default as APIPage } from "./APIPage";
-export { default as Home } from "./Home";
-export { default as Login } from "./Login";
-export { default as LoginHistory } from "./LoginHistory";
-export { default as ForgotPassword } from "./ForgotPassword";
-export { default as Otp } from "./Otp";
-export { default as Signup } from './Signup';
-export { default as UserProfile } from "./UserProfile";
-export { default as SuccessPage } from "./SuccessPage";
-export { default as DevDashboard } from "./DevDashboard/DevDashboard";
-export { default as Analytics } from "./Analytics/Analytics";
+import React from "react";
+
+const APIPage = React.lazy(() => import("./APIPage"))
+const Analytics = React.lazy(() => import("./Analytics/Analytics"))
+const DevDashboard = React.lazy(() => import("./DevDashboard/DevDashboard"))
+const ForgotPassword = React.lazy(() => import("./ForgotPassword"))
+const Home = React.lazy(() => import("./Home"))
+const Login = React.lazy(() => import("./Login"))
+const LoginHistory = React.lazy(() => import("./LoginHistory"))
+const Otp = React.lazy(() => import("./Otp"))
+const Signup = React.lazy(() => import('./Signup'))
+const SuccessPage = React.lazy(() => import("./SuccessPage"))
+const UserProfile = React.lazy(() => import("./UserProfile"))
+
+
+export { APIPage, Analytics, DevDashboard,
+    ForgotPassword, Home, Login,
+    LoginHistory, Otp, Signup,
+    SuccessPage, UserProfile}
