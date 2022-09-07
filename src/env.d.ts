a010037f5843adf69db6977807c06d158b7e1282
temitopefa <temitope.faluta@zummitafrica.com>
2022-09-07 17:27:22 +0100

    fixed env

--- src/env.d.ts
+++
@@ -1,4 +0,0 @@
-VITE_IDENTITY_URL=http://3.87.119.169
-VITE_AI_URL = "https://qnanswer-api.pk25mf6178910.eu-west-3.cs.amazonlightsail.com/q_and_a"
-VITE_SOCKET_URL="http://localhost:3000"
-NODE_OPTIONS="--max-old-space-size=512" 
