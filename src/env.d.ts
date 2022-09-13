755e7f3eb1559e7bec3c8806d7542fbb59a60f3f
CreedTech <ayodeleayoolaoke4@gmail.com>
2022-09-13 21:23:31 +0100

    changed env url

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,4 +2,4 @@
 VITE_BASE_URL="http://18.207.143.26:3000/api"
 VITE_IDENTITY_URL="http://18.207.143.26:3001/api-hub"
 VITE_AI_URL = "https://qnanswer-api.pk25mf6178910.eu-west-3.cs.amazonlightsail.com/q_and_a"
-VITE_SOCKET_URL="http://localhost:3000"
+VITE_SOCKET_URL="http://3.87.119.169.5000/"
