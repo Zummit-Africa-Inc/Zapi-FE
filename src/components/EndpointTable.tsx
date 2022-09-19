d7688be0d92e65536aeb8666d89ee661c14819b8
Vinyl-Davyl <okononfuadavid@gmail.com>
2022-09-19 09:53:48 +0100

    Clean Up

--- src/components/EndpointTable.tsx
+++ src/components/EndpointTable.tsx
@@ -13,32 +13,31 @@ import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { Link } from 'react-router-dom';
 
 function createData(
   name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-  price: number,
+  methodname: number | any,
+  actionedit: number | any,
+  actioncopy: number | any,
+  actionmoveto: number | any,
+  price: number | any,
 ) {
   return {
     name,
-    calories,
-    fat,
-    carbs,
-    protein,
+    methodname,
+    actionedit,
+    actioncopy,
+    actionmoveto,
     price,
     history: [
       {
-        date: '2020-01-05',
-        customerId: '11091700',
-        amount: 3,
+        group: 'Get Categories',
+        method: 'GET',
       },
       {
-        date: '2020-01-02',
-        customerId: 'Anonymous',
-        amount: 1,
+        group: 'Get Product in Category',
+        method: 'GET',
       },
     ],
   };
@@ -69,37 +68,41 @@ function Row(props: { row: ReturnType<typeof createData> }) {
         <TableCell component="th" scope="row">
           {row.name}
         </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell>
+        <TableCell align="right">{row.methodname}</TableCell>
+        <TableCell align="right">{row.actionedit}</TableCell>
+        <TableCell align="right">{row.actioncopy}</TableCell>
+        <TableCell align="right">{row.actionmoveto}</TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
-              </Typography>
               <Table size="small" aria-label="purchases">
                 <TableHead>
                   <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Customer</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>Group</TableCell>
+                    <TableCell align="right">Method</TableCell>
+                    <TableCell align="right">Action</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
                   {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
+                    <TableRow>
                       <TableCell component="th" scope="row">
-                        {historyRow.date}
+                        <Checkbox
+                          color="primary"
+                          inputProps={{
+                            'aria-label': 'select all',
+                          }}
+                        />
                       </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
+                      <TableCell>{historyRow.group}</TableCell>
+                      <TableCell align="right" sx={{color: 'green'}}>{historyRow.method}</TableCell>
                       <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
+                        <Link to="#">
+                          Edit
+                        </Link>
                       </TableCell>
                     </TableRow>
                   ))}
@@ -114,11 +117,11 @@ function Row(props: { row: ReturnType<typeof createData> }) {
 }
 
 const rows = [
-  createData('Catalog', 159, 6.0, 24, 4.0, 3.99),
-  createData('Order', 237, 9.0, 37, 4.3, 4.99),
-  createData('Authentication', 262, 16.0, 24, 6.0, 3.79),
-  createData('Catalog', 305, 3.7, 67, 4.3, 2.5),
-  createData('Auth', 356, 16.0, 49, 3.9, 1.5),
+  createData('Catalog', '', 'Edit', 'Copy', 'Move to', ''),
+  createData('Order', '', 'Edit', 'Copy', 'Move to', ''),
+  createData('Authentication', '', 'Edit', 'Copy', 'Move to', ''),
+  createData('Catalog', '', 'Edit', 'Copy', 'Move to', ''),
+  createData('Auth', '', 'Edit', 'Copy', 'Move to', ''),
 ];
 
 export default function CollapsibleTable() {
