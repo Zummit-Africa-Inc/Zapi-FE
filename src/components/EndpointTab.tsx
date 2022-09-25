2aa03f827903fa7c5f40deaf2b8191abf08e3a47
Samson Okunola <smsnmicheal@gmail.com>
2022-09-25 21:20:44 +0100

    create endpoint functionality added

--- src/components/EndpointTab.tsx
+++ src/components/EndpointTab.tsx
@@ -1,47 +1,76 @@
-import { Button, Paper, Typography } from "@mui/material"
-import { EndpointTable, InputSearch } from "."
-import { makeStyles } from "@mui/styles"
+import React, { FormEvent, useState } from "react";
+import { Paper, Stack, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { toast } from "react-toastify";
 
-import { Add } from "@mui/icons-material"
-import { Link } from "react-router-dom"
+import { EndpointTable, InputSearch } from "./";
+import { useFormInputs } from "../hooks";
+import { mockEndpoint } from './mockdata' // test API
+
+const initialState = { name: '', route: '', method: 'GET' }
 
 const EndpointTab: React.FC = () => {
+    const { inputs, bind, select } = useFormInputs(initialState)
+    const { name, route, method } = inputs
     const classes = useStyles()
+    const [isAdding, setIsAdding] = useState<boolean>(false)
+
+    const toggleState = () => setIsAdding(prev => !prev)
+
+    const addEndpoint = (e: FormEvent) => {
+        e.preventDefault()
+
+        if(!name || !route) return toast.error("Please add a name and route")
+        const payload = { id: Math.random().toFixed(10).toString(), name, route, method }
+        // console.log(payload)
+        // * there will be a post and background get request whenever a new endpoint is added
+        mockEndpoint.unshift(payload)
+        setIsAdding(false)
+    }
+
     return (
         <Paper elevation={1} className={classes.paper}>
-            <>
-            <div className={classes.container}>
-                {/* Api Definition */}
-                <div>
-                    <Typography variant="body1" fontSize="20px" fontWeight={800}>API Definition</Typography>
-                </div>
-                <div className={classes.pageSubHeading}>
-                    <Typography variant="subtitle2" width="650px" fontWeight={400}>When publishing an API to the ZapiAPI Hub, you can either manually edit endpoint definitions, use a specification file.</Typography>
-                </div>
-                {/* Endpoints */}
-                <Typography variant="body1" fontSize="24px" color="rgb(123, 123, 194)" fontWeight={500} mt={2}>Endpoints</Typography>
-                <Typography variant="body1" fontSize="16px" fontWeight={400} mb={1}>Changes made to the endpoints will be reflected in the Hub.</Typography>
-
-                <div className={classes.pageDescription}>
-                    <Typography>Add and define your API endpoints.</Typography>
-                </div>
-                <div className={classes.pageActions}>
-                    <div className={classes.searchContainer}>
-                        <InputSearch className={classes.searchEndpoints} type="text" name="queryString" placeholder="Search Endpoints" />
-                    </div>
-                    <div className={classes.pageButtons}>
-                            <Link to="/create-endpoint" className={classes.createButton}>
-                                <Add sx={{ fontSize: "1rem", color: "rgb(139, 246, 236)", backgroundColor: "#fff", borderRadius: "50%", border: "2px solid rgb(149, 236, 236)"}} />
-                                <Typography sx={{fontWeight: 600, marginLeft: ".5rem"}}>Create Endpoint</Typography>
-                            </Link>
-                        <div className="delete-button">
-                            <Button disabled variant="contained" sx={{backgroundColor: "red", color: "#000"}}>Delete</Button>
-                        </div>
-                    </div>
-                </div><br/><br/>
+            <Stack direction="column" mb={8}>
+            <div>
+                <Typography variant="body1" fontSize="20px" fontWeight={800}>API Definition</Typography>
             </div>
+            <div className={classes.pageSubHeading}>
+                <Typography variant="subtitle2" width="650px" fontWeight={400}>When publishing an API to the ZapiAPI Hub, you can either manually edit endpoint definitions, use a specification file.</Typography>
+            </div>
+            {/* Endpoints */}
+            <Typography variant="body1" fontSize="24px" color="rgb(123, 123, 194)" fontWeight={500} mt={2}>Endpoints</Typography>
+            <Typography variant="body1" fontSize="16px" fontWeight={400} mb={1}>Changes made to the endpoints will be reflected in the Hub.</Typography>
+
+            <div className={classes.pageDescription}>
+                <Typography>Add and define your API endpoints.</Typography>
+            </div>
+            <div className={classes.pageActions}>
+                <input type="text" name="search" className={classes.inputs} placeholder="Search..." />
+                <div>
+                    <button onClick={toggleState} className={classes.button} style={{background: isAdding ? "#E32C08" : "#058A04",}}>
+                        {isAdding ? 'Cancel' : 'Create Endpoint'}
+                    </button>
+                </div>
+            </div>
+            </Stack>
+            {isAdding && (
+                <form onSubmit={addEndpoint}>
+                <Stack direction="row" alignItems="center" spacing={4} my={4}>
+                    <input type="text" name="name" {...bind} className={classes.inputs} placeholder="Name" />
+                    <select name="method" id="" {...select} className={classes.inputs}>
+                        <option value="GET">GET</option>
+                        <option value="POST">POST</option>
+                        <option value="PUT">PUT</option>
+                        <option value="DELETE">DELETE</option>
+                    </select>
+                    <input type="text" name="route" {...bind} className={classes.inputs} placeholder="Route" />
+                    <button type="submit" className={classes.button} style={{background: "#058A04",}}>
+                        Add
+                    </button>
+                </Stack>
+            </form>
+            )}
             <EndpointTable />
-            </>
         </Paper>
     )
 }
@@ -51,9 +80,7 @@ export default EndpointTab
 const useStyles = makeStyles({
     paper: {
         width: "950px",
-        marginTop: "20px"
-    },
-    container: {
+        marginTop: "20px",
         padding: "2rem 2rem",
     },
     pageSubHeading: {
@@ -67,40 +94,38 @@ const useStyles = makeStyles({
         alignItems: "center",
         justifyContent: "space-between"
     },
-    searchContainer: {
-        background: "#fff",
-        color: "grey",
-        padding: ".2rem .4rem",
-        border: "1px solid #ccc",
-        borderRadius: "3px",
-        width: "300px",
-        "&:hover": {
-            border: "1px solid rgb(139, 236, 236)"
-        }
-    },
-    searchEndpoints: {
+    inputs: {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        "& input": {
-            border: "none",
-            outline: "none",
-            width: "300px",
-            paddingLeft: "1rem",
-            fontSize: ".8rem",
-            fontFamily: "var(--body-font)",
+        height: "40px",
+        borderRadius: "4px",
+        border: "1px solid #999",
+        outline: "none",
+        padding: "0 0.5rem",
+        fontWeight: 400,
+        fontSize: "14px",
+        lineHeight: "24px",
+        fontFamily: "var(--body-font)",
+    },
+    button: {
+        display: "flex",
+        alignItems: "center",
+        gap: "0.25rem",
+        color: "#FFF",
+        padding: "6px 16px",
+        borderRadius: "5px",
+        border: "none",
+        outline: "none",
+        fontSize: "0.875rem",
+        fontWeight: 500,
+        lineHeight: 1.75,
+        textTransform: "uppercase",
+        fontFamily: "var(--body-font)",
+        cursor: "pointer",
+        "& disabled": {
+            background: "#E0E0E0",
+            color: "#484848",
         }
     },
-    pageButtons: {
-        display: "flex",
-        alignItems: "center",
-        gap: "1rem",
-    },
-    createButton: {
-        cursor: "pointer",
-        display: "flex",
-        alignItems: "center",
-        color: "#000"
-    }
 })
-
