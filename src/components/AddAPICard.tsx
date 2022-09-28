9e1cbf5da26d564cbd306cf4d49c6e24ec3d7ab4
Vinyl-Davyl <okononfuadavid@gmail.com>
2022-09-28 09:46:42 +0100

    AddAPICard

--- src/components/AddAPICard.tsx
+++ src/components/AddAPICard.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
 import { blue } from '@mui/material/colors';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { Avatar, Paper, Stack } from "@mui/material";
-import { AccountTree, Animation } from "@mui/icons-material";
+import { Avatar, CardHeader, IconButton, Paper, Box, Card, CardContent, Typography } from "@mui/material";
+import { Animation, MoreVertRounded } from "@mui/icons-material";
+
+
 
 
 const AddAPICard: React.FC = () => {
@@ -18,11 +16,18 @@ const AddAPICard: React.FC = () => {
                 <Card variant="outlined">
                     <React.Fragment>
                         <CardContent>
-                        <Stack direction="row" spacing={2}>
-                            <Avatar sx={{ bgcolor: blue[500], mb: 1 }}>
-                                <Animation />
-                            </Avatar>
-                        </Stack>
+                            <CardHeader
+                                avatar={
+                                <Avatar sx={{ bgcolor: blue[500], mb: 1, mr: 5, ml: -2 }}>
+                                    <Animation />
+                                </Avatar>
+                                }
+                                action={
+                                <IconButton aria-label="settings">
+                                    <MoreVertRounded />
+                                </IconButton>
+                                }
+                            />
                             <Typography variant="h5" component="div" sx={{  fontSize: "18px", fontWeight: "500", mb: 1 }}>
                                 👋 Onboarding Project
                             </Typography>
@@ -40,24 +45,9 @@ const AddAPICard: React.FC = () => {
     );
 };
 
+
 const useStyles = makeStyles({
-    // root: {
-    //     width: 300,
-    //     height: 320,
-    //     padding: "1rem 0.5rem",
-    // },
-    // span: {
-    //     display: "flex",
-    //     alignItems: "center",
-    //     justifyContent: "center",
-    //     gap: "0.5rem",
-    // },
-    // card: {
-    //     transition: "0.2s all ease-in-out",
-    //     "&:hover": {
-    //         transform: "scale(1.03)",
-    //     }
-    // }
+   
 });
 
 export default AddAPICard;
