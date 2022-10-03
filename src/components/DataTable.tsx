8409492866246e760ddd4510d95ac8ff9b8b62f0
lasbohoo <temitopefaluta@gmail.com>
2022-10-04 03:12:56 +0530

    invitees

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,5 +1,6 @@
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel, TablePagination } from '@mui/material';
 import React, { useState } from 'react';
+import { makeStyles } from "@mui/styles"
 
 interface TableProps {
   Heading: Array<string>
@@ -9,6 +10,7 @@ interface TableProps {
 const DataTable: React.FC<TableProps> = ({Heading, Rows}) => {
   const [page, setPage] = useState<number>(0);
   const [rowsPerPage, setRowsPerPage] = useState<number>(5);
+  const classes = useStyles();
 
     const Keys = Object.keys(Rows[0])
     const result = [...Rows.map(obj => Keys.map(key => obj[key]))];
@@ -26,7 +28,7 @@ const DataTable: React.FC<TableProps> = ({Heading, Rows}) => {
     <div className='datatable'>
       <TableContainer component={Paper} className="table">
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
+          <TableHead className={classes.head}>
             <TableRow>{Heading.map((heading) => (
                 <TableCell className="tableCell" key={heading}>{heading}</TableCell>
             ))}
@@ -56,5 +58,13 @@ const DataTable: React.FC<TableProps> = ({Heading, Rows}) => {
     </div>
   )
 }
+
+const useStyles = makeStyles({
+  head:{
+    "&.MuiTableHead-root":{
+      backgroundColor:'rgba(0, 0, 0, 0.38)'
+    }
+  }
+})
 
 export default DataTable
