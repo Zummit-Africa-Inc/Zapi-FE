305b3fffd260672b33e2b6077d53ba2a66462f9a
timmy_3000 <hatimk916@gmail.com>
2022-10-11 12:45:11 +0200

    Create APIHub.tsx

---
+++ src/pages/APIHub.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { HomeNavbar, APIHubHeader, Footer } from '../sections'
+
+
+const Pricing:React.FC = () => {
+
+    return (
+        <>
+            <HomeNavbar />
+            <APIHubHeader />
+            <Footer />
+        </>
+    )
+}
+
+
+export default Pricing;
