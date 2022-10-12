d8d88a24de50777d8b2fb7b9babd4ecb78f421e7
Alan-Turing-Codes <davidbradforduko@yahoo.com>
2022-10-12 15:49:52 +0100

    api hub tab content added

--- src/components/AllApiCard.tsx
+++ src/components/AllApiCard.tsx
@@ -5,7 +5,7 @@ import { makeStyles } from '@mui/styles';
 import { Box, Stack } from '@mui/material';
 
 
-const ApiCard = () => {
+const AllApiCard = () => {
 
   const classes = useStyles();
 
@@ -469,7 +469,7 @@ const ApiCard = () => {
   );
 }
 
-export default ApiCard;
+export default AllApiCard;
 
 const useStyles = makeStyles({
   container:{
