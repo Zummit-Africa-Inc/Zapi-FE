35a592ce796236c2e96bf2beaa87c3b1742c866e
creedtech <ayodeleayolaoke4@gmail.com>
2022-10-12 20:47:00 +0100

    completed

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from 'react-redux'
 import { makeStyles } from "@mui/styles";
 import { Badge, Menu, MenuItem } from '@mui/material';
 import { Link } from "react-router-dom";
