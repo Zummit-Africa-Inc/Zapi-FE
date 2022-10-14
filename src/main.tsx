f7ffedd333c7dabf93154f523cce2cdf7a5d1ff7
Brain Philemon <philbrainy@gmail.com>
2022-10-14 16:53:49 +0100

    feat: amplify setup for API calls

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,33 @@ import App from "./App";
 import "./index.css";
 import { store } from "./redux/store";
 import { ContextProvider } from "./contexts/ContextProvider";
+import { Amplify } from "aws-amplify";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+Amplify.configure({
+  API: {
+    endpoints: [
+      {
+        name: "VITE_IDENTITY_URL",
+        endpoint: "https://identity.zapi.ai/api/v1",
+      },
+      {
+        name: "VITE_AI_URL",
+        endpoint:
+          "https://qnanswer-api.pk25mf6178910.eu-west-3.cs.amazonlightsail.com/q_and_a",
+      },
+      {
+        name: "VITE_SOCKET_URL",
+        endpoint: "https://notification.zapi.ai/api/v1",
+      },
+      {
+        name: "VITE_CORE_URL",
+        endpoint: "https://core.zapi.ai/api/v1",
+      },
+    ],
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
