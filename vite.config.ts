41677fc73011ca186b27fa18766b2b7d6bcc5885
Brain Philemon <philbrainy@gmail.com>
2022-10-14 20:22:49 +0100

    bug: final vite fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  define : {
-    global: {}
-  },
-  build: {
-    rollupOptions : {
-      external: "jss-plugin-{}"
-    }
-  },
   resolve: {
     alias: {
       './runtimeConfig': './runtimeConfig.browser',
