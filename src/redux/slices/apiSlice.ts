631f8948c7550ffdc427bb6cf77c0dff0010082d
Samson Okunola <smsnmicheal@gmail.com>
2022-10-18 12:29:03 +0100

    fix getApiCategories

--- src/redux/slices/apiSlice.ts
+++ src/redux/slices/apiSlice.ts
@@ -2,22 +2,34 @@ import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 import { APIType } from "../../types";
 
-const url = import.meta.env.VITE_CORE_URL
+const core_url = import.meta.env.VITE_CORE_URL
 interface ApiState {
     apis: Array<APIType>
+    categories: Array<APIType>
     loading: "idle" | "pending" | "fulfilled" | "rejected"
     error?: any
 }
 
 const initialState = {
     apis: [],
+    categories: [],
     loading: "idle",
     error: null
 } as ApiState
 
+export const getApiCagetories = createAsyncThunk("apis/getApiCategories", async(_, thunkAPI) => {
+    try {
+        const response = await fetch(`${core_url}/categories`)
+        const data = await response.json()
+        return data.data
+    } catch (error: any) {
+        return thunkAPI.rejectWithValue(error.message)
+    }
+})
+
 export const getApis = createAsyncThunk("apis/getApis", async(_, thunkAPI) => {
     try {
-        const response = await fetch(`${url}/categories`)
+        const response = await fetch(`${core_url}/api`)
         const data = await response.json()
         return data.data
     } catch (error: any) {
@@ -42,6 +54,17 @@ const apiSlice = createSlice({
             state.loading = "fulfilled"
         }),
         builder.addCase(getApis.rejected, (state, action: PayloadAction<any>) => {
+            state.loading = "rejected"
+            state.error = action.payload
+        }),
+        builder.addCase(getApiCagetories.pending, (state) => {
+            state.loading = "pending"
+        }),
+        builder.addCase(getApiCagetories.fulfilled, (state, action: PayloadAction<any>) => {
+            state.categories = action.payload
+            state.loading = "fulfilled"
+        }),
+        builder.addCase(getApiCagetories.rejected, (state, action: PayloadAction<any>) => {
             state.loading = "rejected"
             state.error = action.payload
         })
