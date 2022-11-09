6952f60fbf2391f63e0d1c19487e4646ee143270
Charles <egualecharles@gmail.com>
2022-11-10 00:24:40 +0100

    change feedback button to icon

--- src/sections/Feedback.tsx
+++ src/sections/Feedback.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import MessageIcon from '@mui/icons-material/Message';
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 
 const Feedback = ({handleOpen} : any) => {
     return (
     <Box sx={feedback}>
       <Button onClick={handleOpen}>
-        <Typography marginRight={0.5} fontWeight="600">Feedback</Typography>
-        <MessageIcon fontSize="medium"/>
+        <ChatOutlinedIcon fontSize="large" sx={{fontSize: "4em", background: "white", borderRadius: "30%"}}/>
       </Button>
     </Box>
     )
@@ -18,9 +17,8 @@ export default Feedback;
 
 const feedback = {
     position: "fixed",
-    bottom: "1%",
+    bottom: "0%",
     right: "1%",
-    background: "yellow",
     "@media screen and (max-width: 400px)": {
       position: "fixed",
       bottom: "1%",
