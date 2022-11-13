b42c2e9f96f58cf30e34f418e9f466f61edc0f06
Charles <egualecharles@gmail.com>
2022-11-14 00:07:06 +0100

    change icon

--- src/sections/Feedback.tsx
+++ src/sections/Feedback.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+import FeedbackOutlinedIcon from '@mui/icons-material/FeedbackOutlined';
 
 const Feedback = ({handleOpen} : any) => {
     return (
     <Box sx={feedback}>
       <Button onClick={handleOpen}>
-        <ChatOutlinedIcon fontSize="large" sx={{fontSize: "4em", background: "white", borderRadius: "30%"}}/>
+        <FeedbackOutlinedIcon fontSize="large" sx={{fontSize: "4em", background: "white", borderRadius: "30%"}}/>
       </Button>
     </Box>
     )
