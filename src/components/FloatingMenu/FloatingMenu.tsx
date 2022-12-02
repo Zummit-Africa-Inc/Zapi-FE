4415da574861d7da093a0301e95814a2c63ffe1f
Brain Philemon <philbrainy@gmail.com>
2022-12-02 17:01:06 +0100

    bug: flex direction enum

--- src/components/FloatingMenu/FloatingMenu.tsx
+++ src/components/FloatingMenu/FloatingMenu.tsx
@@ -1,88 +1,85 @@
-import React from 'react';
+import React from "react";
 import { makeStyles } from "@mui/styles";
-import { Fab } from '@mui/material';
-import { List } from '@mui/material';
+import { Fab } from "@mui/material";
+import { List } from "@mui/material";
 
 export const DIRECTIONS = {
-    up: 'column-reverse',
-    down: 'column',
-    left: 'row-reverse',
-    right: 'row',
+  up: "column-reverse",
+  down: "column",
+  left: "row-reverse",
+  right: "row",
 };
 
-
 export enum Directions {
-    Up = 'up',
-    Down = 'down',
-    Left = 'left',
-    Right = 'right'
+  Up = "up",
+  Down = "down",
+  Left = "left",
+  Right = "right",
 }
 
 export interface FloatingMenuProps {
-    children: JSX.Element[] | JSX.Element | string
-    spacing?: number
-    slideSpeed?: number
-    direction?: Directions
-    isOpen: boolean
+  children: JSX.Element[] | JSX.Element | string;
+  spacing?: number;
+  slideSpeed?: number;
+  direction?: Directions;
+  isOpen: boolean;
 }
 
 const FloatingMenu = ({
-    slideSpeed = 500,
-    direction = Directions.Down,
-    isOpen = false,
-    spacing = 8,
-    children,
-    ...rest
+  slideSpeed = 500,
+  direction = Directions.Down,
+  isOpen = false,
+  spacing = 8,
+  children,
+  ...rest
 }: FloatingMenuProps) => {
-    const childrenWithProps = React.Children.map(
-        children,
-        (child: any, index: number) =>
-            React.cloneElement(child, {
-                isOpen,
-                slideSpeed,
-                direction,
-                index,
-                spacing
-            })
-    )
+  const childrenWithProps = React.Children.map(
+    children,
+    (child: any, index: number) =>
+      React.cloneElement(child, {
+        isOpen,
+        slideSpeed,
+        direction,
+        index,
+        spacing,
+      })
+  );
 
-    return (
-        <Fab color="primary" >
-            <List sx={listStyle(direction)} {...rest}>
-                {childrenWithProps}
-            </List>
-        </Fab>
-    )
-}
+  return (
+    <Fab color="primary">
+      <List sx={listStyle(direction)} {...rest}>
+        {childrenWithProps}
+      </List>
+    </Fab>
+  );
+};
 
-export default FloatingMenu
+export default FloatingMenu;
 
-
-
-const listStyle = (direction) => {
-    return {
-        position: "fixed",
-        bottom: "1%",
-        right: "1%",
-        display: 'flex',
-        width: 'fit-content',
-        listStyle: 'none',
-        margin: '0',
-        padding: '0',
-        flexDirection: DIRECTIONS[direction],
-        justifyContent: 'center',
-        alignItems: 'center',
-        animation: `$pulse 1s infinite`,
-        '@keyframes pulse': {
-            '0%': {
-                transform: 'scale(1)',
-            },
-            '50%': {
-                transform: 'scale(1.1)',
-            },
-            '100%': {
-                transform: 'scale(1)',
-            },
-        },
-    }
-}
+const listStyle = (direction: Directions = Directions.Down) => {
+  return {
+    position: "fixed",
+    bottom: "1%",
+    right: "1%",
+    display: "flex",
+    width: "fit-content",
+    listStyle: "none",
+    margin: "0",
+    padding: "0",
+    flexDirection: DIRECTIONS[direction],
+    justifyContent: "center",
+    alignItems: "center",
+    animation: `$pulse 1s infinite`,
+    "@keyframes pulse": {
+      "0%": {
+        transform: "scale(1)",
+      },
+      "50%": {
+        transform: "scale(1.1)",
+      },
+      "100%": {
+        transform: "scale(1)",
+      },
+    },
+  };
+};
